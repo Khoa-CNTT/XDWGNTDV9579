69152ae24af7e002cd59044363219d1d2d0d7910
Bảo Nguyên <tn2842003@gmail.com>
2025-04-04 11:27:16 +0700

    page Admin

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 // src/App.js
-import { Routes, Route } from "react-router-dom"; // Loại bỏ BrowserRouter
+import { useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
 import { AuthProvider, useAuth } from "./context/AuthContext";
 import "./App.css";
 import Home from "./pages/Home/Home";
@@ -19,56 +22,150 @@ import CartPage from "./pages/Cart/CartPage";
 import Chatbox from "./components/Chatbox/ChatBox";
 import PrivateRoute from "./components/Common/PrivateRoute";
 
+// Admin Components
+import Topbar from "./Admin/global/Topbar";
+import Sidebar from "./Admin/global/Sidebar";
+import Dashboard from "./Admin/dashboard";
+import Team from "./Admin/team";
+import Invoices from "./Admin/invoices";
+import Contacts from "./Admin/contacts";
+import Bar from "./Admin/bar";
+import Form from "./Admin/form";
+import Line from "./Admin/line";
+import Pie from "./Admin/pie";
+import FAQ from "./Admin/faq";
+import Geography from "./Admin/geography";
+
 function App() {
   const { loading } = useAuth();
+  const location = useLocation();
+  const isAdmin = location.pathname.startsWith("/admin");
+
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
 
   if (loading) {
     return <div className="loading-spinner">Đang tải...</div>;
   }
-
   return (
-    <AuthProvider>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about-us" element={<About />} />
-        <Route path="contact-us" element={<Contact />} />
-        <Route path="tours" element={<Tours />} />
-        <Route path="tour-details" element={<TourDetails />} />
-        <Route
-          path="booking"
-          element={
-            <PrivateRoute>
-              <Booking />
-            </PrivateRoute>
-          }
-        />
-        <Route path="destinations" element={<Destinations />} />
-        <Route path="gallery" element={<PhotoGallery />} />
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-        <Route path="forgot-password" element={<ForgotPassword />} />
-        <Route
-          path="cart"
-          element={
-            <PrivateRoute>
-              <CartPage />
-            </PrivateRoute>
-          }
-        />
-        <Route
-          path="dashboard"
-          element={
-            <PrivateRoute>
-              <div>Dashboard</div>
-            </PrivateRoute>
-          }
-        />
-      </Routes>
-      <Chatbox />
-      <Footer />
-    </AuthProvider>
+    <>
+      {isAdmin ? (
+        // Giao diện Admin
+        <ColorModeContext.Provider value={colorMode}>
+          <ThemeProvider theme={theme}>
+            <CssBaseline />
+            <div className="app">
+              <Sidebar isSidebar={isSidebar} />
+              <main className="content">
+                <Topbar setIsSidebar={setIsSidebar} />
+                <Routes>
+                  <Route path="/admin" element={<Dashboard />} />
+                  <Route path="/admin/team" element={<Team />} />
+                  <Route path="/admin/contacts" element={<Contacts />} />
+                  <Route path="/admin/invoices" element={<Invoices />} />
+                  <Route path="/admin/form" element={<Form />} />
+                  <Route path="/admin/bar" element={<Bar />} />
+                  <Route path="/admin/pie" element={<Pie />} />
+                  <Route path="/admin/line" element={<Line />} />
+                  <Route path="/admin/faq" element={<FAQ />} />
+                  <Route path="/admin/geography" element={<Geography />} />
+                </Routes>
+              </main>
+            </div>
+          </ThemeProvider>
+        </ColorModeContext.Provider>
+      ) : (
+        // Giao diện Client
+        <>
+            <AuthProvider>
+              <Header />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="about-us" element={<About />} />
+                <Route path="contact-us" element={<Contact />} />
+                <Route path="tours" element={<Tours />} />
+                <Route path="tour-details" element={<TourDetails />} />
+                <Route
+                  path="booking"
+                  element={
+                    <PrivateRoute>
+                      <Booking />
+                    </PrivateRoute>
+                  }
+                />
+                <Route path="destinations" element={<Destinations />} />
+                <Route path="gallery" element={<PhotoGallery />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route path="forgot-password" element={<ForgotPassword />} />
+                <Route
+                  path="cart"
+                  element={
+                    <PrivateRoute>
+                      <CartPage />
+                    </PrivateRoute>
+                  }
+                />
+                <Route
+                  path="dashboard"
+                  element={
+                    <PrivateRoute>
+                      <div>Dashboard</div>
+                    </PrivateRoute>
+                  }
+                />
+              </Routes>
+              <Chatbox />
+              <Footer />
+            </AuthProvider>
+         
+        </>
+      )}
+    </>
   );
+  // return (
+  //   <AuthProvider>
+  //     <Header />
+  //     <Routes>
+  //       <Route path="/" element={<Home />} />
+  //       <Route path="about-us" element={<About />} />
+  //       <Route path="contact-us" element={<Contact />} />
+  //       <Route path="tours" element={<Tours />} />
+  //       <Route path="tour-details" element={<TourDetails />} />
+  //       <Route
+  //         path="booking"
+  //         element={
+  //           <PrivateRoute>
+  //             <Booking />
+  //           </PrivateRoute>
+  //         }
+  //       />
+  //       <Route path="destinations" element={<Destinations />} />
+  //       <Route path="gallery" element={<PhotoGallery />} />
+  //       <Route path="login" element={<Login />} />
+  //       <Route path="register" element={<Register />} />
+  //       <Route path="forgot-password" element={<ForgotPassword />} />
+  //       <Route
+  //         path="cart"
+  //         element={
+  //           <PrivateRoute>
+  //             <CartPage />
+  //           </PrivateRoute>
+  //         }
+  //       />
+  //       <Route
+  //         path="dashboard"
+  //         element={
+  //           <PrivateRoute>
+  //             <div>Dashboard</div>
+  //           </PrivateRoute>
+  //         }
+  //       />
+  //     </Routes>
+  //     <Chatbox />
+  //     <Footer />
+  //   </AuthProvider>
+  // );
 }
 
 export default App;
