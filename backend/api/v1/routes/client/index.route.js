81a3c9678b6bebff50a69c75170072447788dd5e
chison2707 <chiphansonzz17@gmail.com>
2025-04-05 16:15:49 +0700

    view detail tour

--- backend/api/v1/routes/client/index.route.js
+++ backend/api/v1/routes/client/index.route.js
@@ -1,6 +1,7 @@
 const userRoute = require("./user.route");
 const homeRoute = require("./home.route");
 const categoryRoute = require("./category.route");
+const tourRoute = require("./tour.route");
 
 module.exports = (app) => {
     const version = "/api/v1";
@@ -8,4 +9,5 @@ module.exports = (app) => {
     app.use(version + '/', homeRoute);
     app.use(version + '/users', userRoute);
     app.use(version + '/categories', categoryRoute);
+    app.use(version + '/tours', tourRoute);
 }
