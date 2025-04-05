d5e0c57a7336d43e4c83e11ff37c45ce787d2bcb
chison2707 <chiphansonzz17@gmail.com>
2025-04-05 15:51:53 +0700

    home client

--- backend/api/v1/routes/client/index.route.js
+++ backend/api/v1/routes/client/index.route.js
@@ -1,7 +1,9 @@
 const userRoute = require("./user.route");
+const homeRoute = require("./home.route");
 
 module.exports = (app) => {
     const version = "/api/v1";
 
+    app.use(version + '/', homeRoute);
     app.use(version + '/users', userRoute);
 }
