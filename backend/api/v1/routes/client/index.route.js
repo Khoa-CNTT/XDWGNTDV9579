bfaee6902a4a386dafc9ce37a6db3c0d5bee7adc
chison2707 <chiphansonzz17@gmail.com>
2025-04-05 16:13:46 +0700

    view categiory

--- backend/api/v1/routes/client/index.route.js
+++ backend/api/v1/routes/client/index.route.js
@@ -1,9 +1,11 @@
 const userRoute = require("./user.route");
 const homeRoute = require("./home.route");
+const categoryRoute = require("./category.route");
 
 module.exports = (app) => {
     const version = "/api/v1";
 
     app.use(version + '/', homeRoute);
     app.use(version + '/users', userRoute);
+    app.use(version + '/categories', categoryRoute);
 }
