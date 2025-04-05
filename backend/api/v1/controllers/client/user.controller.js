8bdf6da7b160fb8dd6e9e14aa26cbc85144ec47c
chison2707 <chiphansonzz17@gmail.com>
2025-04-05 16:24:14 +0700

    final user

--- backend/api/v1/controllers/client/user.controller.js
+++ backend/api/v1/controllers/client/user.controller.js
@@ -2,6 +2,7 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const User = require("../../models/user.model");
 const generateHelper = require("../../helpers/generate");
+const sendMailHelper = require("../../helpers/sendMail");
 const Cart = require('../../models/cart.model');
 
 // [POST]/api/v1/users/register
@@ -84,3 +85,146 @@ module.exports.login = async (req, res) => {
         cartId: cart.id
     });
 };
+
+// [POST]/api/v1/users/password/forgot
+module.exports.forgotPassword = async (req, res) => {
+    const email = req.body.email;
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    });
+
+    if (!user) {
+        res.json({
+            code: 400,
+            message: "Email không tồn tại"
+        });
+        return;
+    }
+
+    const otp = generateHelper.generateRandomNumber(6);
+
+    const objForgotPassword = {
+        email: email,
+        otp: otp,
+        expireAt: Date.now()
+    };
+
+    const forgotPassword = new ForgotPassword(objForgotPassword);
+    await forgotPassword.save();
+
+    // gửi otp qua email user
+    const subject = "Mã OTP để lấy lại mật khẩu";
+    const html = `
+    Mã OTP để lấy lại mật khẩu của bạn là <b>${otp}</b>.
+    Vui lòng không chia sẻ mã OTP này với bất kỳ ai.
+    `;
+
+    sendMailHelper.sendMail(email, subject, html);
+    res.json({
+        code: 200,
+        message: "Đã gửi mã OTP qua email"
+    });
+};
+
+// [POST]/api/v1/users/password/otp
+module.exports.otpPassword = async (req, res) => {
+    const email = req.body.email;
+    const otp = req.body.otp;
+
+    const result = await ForgotPassword.findOne({
+        email: email,
+        otp: otp
+    });
+
+    if (!result) {
+        res.json({
+            code: 400,
+            message: "Mã OTP không đúng"
+        });
+        return;
+    }
+
+    const user = await User.findOne({
+        email: email
+    });
+
+    const token = user.token;
+    res.cookie("token", token);
+
+    res.json({
+        code: 200,
+        message: "Xác thực thành công!",
+        token: token
+    });
+};
+
+// [POST]/api/v1/users/password/reset
+module.exports.resetPassword = async (req, res) => {
+    const token = req.body.token;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+        token: token
+    });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (isMatch) {
+        res.json({
+            code: 400,
+            message: "Mật khẩu đã tồn tại"
+        });
+        return;
+    }
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    await User.updateOne({
+        token: token
+    }, {
+        password: hashedPassword
+    });
+
+    res.json({
+        code: 200,
+        message: "Đặt lại mật khẩu thành công"
+    });
+};
+
+// [GET]/api/v1/users/detail
+module.exports.detail = async (req, res) => {
+    res.json({
+        code: 200,
+        message: "Thành công",
+        infor: req.user
+    });
+};
+
+// [PATCH]/api/v1/users/edit
+module.exports.edit = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const data = await User.findByIdAndUpdate({
+            _id: userId
+        }, req.body, {
+            new: true
+        });
+        res.json({
+            code: 200,
+            message: "Thành công",
+            data: data
+        });
+    } catch (error) {
+        res.json({
+            code: 500,
+            message: "error: " + error
+        });
+    }
+};
+
+// [GET]/api/v1/user/logout
+module.exports.logout = async (req, res) => {
+    res.clearCookie("token");
+    res.clearCookie("cartId");
+    res.json({
+        code: 200,
+        message: "Đăng xuất thành công"
+    });
+}
