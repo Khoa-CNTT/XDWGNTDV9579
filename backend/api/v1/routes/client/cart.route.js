943d58c67fa74a8f3e57e6bf83a66593f1338129
chison2707 <chiphansonzz17@gmail.com>
2025-04-12 10:16:44 +0700

    route cart

---
+++ backend/api/v1/routes/client/cart.route.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const controller = require("../../controllers/client/cart.controller");
+const authMiddleware = require("../../middlewares/client/auth.middleware");
+
+router.get('/', authMiddleware.requireAuth, controller.index);
+router.post('/add/:tour_id', authMiddleware.requireAuth, controller.addPost);
+router.post('/add/:hotel_id/:room_id', authMiddleware.requireAuth, controller.addPostHotel);
+router.patch('/delete/:tour_id', authMiddleware.requireAuth, controller.delete);
+router.patch('/deleteHotel/:hotel_id/:room_id', authMiddleware.requireAuth, controller.deleteRoom);
+router.patch('/deleteHotel/:hotel_id', authMiddleware.requireAuth, controller.deleteHotel);
+router.patch('/update/:tour_id/:quantity', authMiddleware.requireAuth, controller.update);
+router.patch('/updateRoom/:hotel_id/:room_id/:quantity', authMiddleware.requireAuth, controller.updateRoom);
+
+module.exports = router;
--- backend/api/v1/routes/client/index.route.js
+++ backend/api/v1/routes/client/index.route.js
@@ -2,6 +2,7 @@ const userRoute = require("./user.route");
 const homeRoute = require("./home.route");
 const categoryRoute = require("./category.route");
 const tourRoute = require("./tour.route");
+const cartRoute = require("./cart.route");
 
 module.exports = (app) => {
     const version = "/api/v1";
@@ -10,4 +11,5 @@ module.exports = (app) => {
     app.use(version + '/users', userRoute);
     app.use(version + '/categories', categoryRoute);
     app.use(version + '/tours', tourRoute);
+    app.use(version + '/carts', cartRoute);
 }
