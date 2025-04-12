70049be4629b86a85c009be163d4299e440774bc
chison2707 <chiphansonzz17@gmail.com>
2025-04-12 10:37:01 +0700

    quản lý tour

--- backend/api/v1/routes/admin/index.route.js
+++ backend/api/v1/routes/admin/index.route.js
@@ -2,6 +2,8 @@ const systemConfig = require("../../../../config/system");
 
 const accountRoutes = require("./account.route");
 const hotelRoutes = require("./hotel.route");
+const categoryRoutes = require("./category.route");
+const tourRoutes = require("./tour.route");
 
 const authMiddleware = require("../../middlewares/admin/auth.middleware");
 
@@ -11,4 +13,6 @@ module.exports = (app) => {
 
     app.use(version + PARTH_ADMIN + "/accounts", accountRoutes);
     app.use(version + PARTH_ADMIN + "/hotels", authMiddleware.requireAuth, hotelRoutes);
+    app.use(version + PARTH_ADMIN + "/categories", authMiddleware.requireAuth, categoryRoutes);
+    app.use(version + PARTH_ADMIN + "/tours", authMiddleware.requireAuth, tourRoutes);
 }
