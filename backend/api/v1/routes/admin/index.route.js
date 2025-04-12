5ca4368ecff1818efbf2333a6c98f12c5b5583bf
chison2707 <chiphansonzz17@gmail.com>
2025-04-12 10:26:07 +0700

    account route + middleware

---
+++ backend/api/v1/routes/admin/index.route.js
@@ -0,0 +1,12 @@
+const systemConfig = require("../../../../config/system");
+
+const accountRoutes = require("./account.route");
+
+const authMiddleware = require("../../middlewares/admin/auth.middleware");
+
+module.exports = (app) => {
+    const version = "/api/v1";
+    const PARTH_ADMIN = systemConfig.prefixAdmin;
+
+    app.use(version + PARTH_ADMIN + "/accounts", accountRoutes);
+}
