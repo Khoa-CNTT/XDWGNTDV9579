ec430e0e46b517c242c9b943c7be5c06c04ae902
Bảo Nguyên <tn2842003@gmail.com>
2025-04-14 21:24:25 +0700

    Update api Admin

--- backend/api/v1/routes/admin/index.route.js
+++ backend/api/v1/routes/admin/index.route.js
@@ -5,14 +5,14 @@ const hotelRoutes = require("./hotel.route");
 const categoryRoutes = require("./category.route");
 const tourRoutes = require("./tour.route");
 
-const authMiddleware = require("../../middlewares/admin/auth.middleware");
+// const authMiddleware = require("../../middlewares/admin/auth.middleware");
 
 module.exports = (app) => {
     const version = "/api/v1";
     const PARTH_ADMIN = systemConfig.prefixAdmin;
 
     app.use(version + PARTH_ADMIN + "/accounts", accountRoutes);
-    app.use(version + PARTH_ADMIN + "/hotels", authMiddleware.requireAuth, hotelRoutes);
-    app.use(version + PARTH_ADMIN + "/categories", authMiddleware.requireAuth, categoryRoutes);
-    app.use(version + PARTH_ADMIN + "/tours", authMiddleware.requireAuth, tourRoutes);
+    // app.use(version + PARTH_ADMIN + "/hotels", authMiddleware.requireAuth, hotelRoutes);
+    // app.use(version + PARTH_ADMIN + "/categories", authMiddleware.requireAuth, categoryRoutes);
+    // app.use(version + PARTH_ADMIN + "/tours", authMiddleware.requireAuth, tourRoutes);
 }
